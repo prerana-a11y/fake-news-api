58323a0bb8f51d0e9343ac47a33b06eda6a2ecc3
prerana-a11y <dashprerana52@gmail.com>
2026-02-03 14:44:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import pickle
+import os
 
 app = Flask(__name__)
+CORS(app)
 
 # Load model and vectorizer
 model = pickle.load(open("model.pkl", "rb"))
